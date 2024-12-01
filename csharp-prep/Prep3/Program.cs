68f9ab1a2e9f06facc4152a986582271ba7a780c
crispol12 <cristian.paul14@gmail.com>
2024-11-30 22:49:08 -0500

    change program 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,59 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        string playAgain = "yes";
+
+        while (playAgain == "yes")
+        {
+            int magicNumber = randomGenerator.Next(1, 101);
+            int guess = -1;
+            int guessCount = 0;
+
+            Console.WriteLine("\nWelcome to the 'Guess My Number' game!");
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                string userInput = Console.ReadLine();
+
+                bool isValidNumber = int.TryParse(userInput, out guess);
+
+                if (!isValidNumber)
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                    continue;
+                }
+
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! The number was {magicNumber}.");
+                    Console.WriteLine($"It took you {guessCount} tries.");
+                }
+            }
+
+            Console.Write("Do you want to play again? (yes/no) ");
+            playAgain = Console.ReadLine().ToLower();
+
+            while (playAgain != "yes" && playAgain != "no")
+            {
+                Console.Write("Please answer 'yes' or 'no': ");
+                playAgain = Console.ReadLine().ToLower();
+            }
+        }
+
+        Console.WriteLine("Thanks for playing! See you next time!");
     }
 }
