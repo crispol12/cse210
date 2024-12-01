c28830717e3da31e92dc5f37bb36cf9a38c9c133
crispol12 <cristian.paul14@gmail.com>
2024-11-30 21:52:32 -0500

    change program 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,16 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Solicitar el apellido
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Mostrar el resultado en el formato deseado
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
